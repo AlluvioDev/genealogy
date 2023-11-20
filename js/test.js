f13fddbfb9d62db7f66cf5ef6186b0ccf0c5202f
4eDo <61490588+4eDo@users.noreply.github.com>
2023-11-20 18:05:11 +0300

    Update test.js

--- js/test.js
+++ js/test.js
@@ -12,12 +12,6 @@ const cellsInRow = nodesInRow * 2 - 1;
 const cellsInColumn = nodesInColumn * 2 - 1;
 const cellSize = 30;
 
-var scale = 1,
-	panning = false,
-	pointX = 0,
-	pointY = 0,
-	start = { x: 0, y: 0 },
-	zoom = document.getElementById("zoom");
 
 async function loadPersons() {
 	const response = await fetch(personsSrc);
@@ -253,40 +247,4 @@ function drawPersonInCell(row_id, col_id, person_id) {
 		.replace("{{FIRST_NAME}}", person["firstName"]);
 	drawInCell(row_id, col_id, personCell);
 	return;
-}
-
-function setTransform() {
-	zoom.style.transform = "translate(" + pointX + "px, " + pointY + "px) scale(" + scale + ")";
-}
-
-zoom.onmousedown = function (e) {
-	e.preventDefault();
-	start = { x: e.clientX - pointX, y: e.clientY - pointY };
-	panning = true;
-}
-
-zoom.onmouseup = function (e) {
-	panning = false;
-}
-
-zoom.onmousemove = function (e) {
-	e.preventDefault();
-	if (!panning) {
-		return;
-	}
-	pointX = (e.clientX - start.x);
-	pointY = (e.clientY - start.y);
-	setTransform();
-}
-
-zoom.onwheel = function (e) {
-	e.preventDefault();
-	var xs = (e.clientX - pointX) / scale,
-		ys = (e.clientY - pointY) / scale,
-		delta = (e.wheelDelta ? e.wheelDelta : -e.deltaY);
-	(delta > 0) ? (scale *= 1.2) : (scale /= 1.2);
-	pointX = e.clientX - xs * scale;
-	pointY = e.clientY - ys * scale;
-
-	setTransform();
 }
